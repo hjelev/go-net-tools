5bc26b0e5b348da587df33e4537a306a97741ca7
Hristo <jeleff@gmail.com>
2022-10-19 23:50:39 +0300

    minor update

--- check_ssl/check_ssl.go
+++ check_ssl/check_ssl.go
@@ -24,8 +24,8 @@ func main() {
 			expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
 			fmt.Printf("Issuer: %s\nExpiry: %v\n", conn.ConnectionState().PeerCertificates[0].Issuer, expiry.Format(time.RFC850))
 		}
-	} else{
+	} else {
 		fmt.Println("Usage: check_ssl domain.tld")
-	 }
+	}
 
 }
