95ee45112e792d1789d953cc0f24b71e61c54793
Hristo <jeleff@gmail.com>
2022-10-19 23:39:12 +0300

    minor update

--- check_ssl/check_ssl.go
+++ check_ssl/check_ssl.go
@@ -13,17 +13,17 @@ func main() {
 		domain := os.Args[1]
 		conn, err := tls.Dial("tcp", domain + ":443", nil)
 		if err != nil {
-			panic("Server doesn't support SSL certificate err: " + err.Error())
-		}
+			fmt.Println("Server doesn't support SSL certificate err: " + err.Error())
+		} else {
 
-		err = conn.VerifyHostname(domain)
-		if err != nil {
-			panic("Hostname doesn't match with certificate: " + err.Error())
+			err = conn.VerifyHostname(domain)
+			if err != nil {
+				panic("Hostname doesn't match with certificate: " + err.Error())
+			}
+			
+			expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
+			fmt.Printf("Issuer: %s\nExpiry: %v\n", conn.ConnectionState().PeerCertificates[0].Issuer, expiry.Format(time.RFC850))
 		}
-		
-		expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
-		fmt.Printf("Issuer: %s\nExpiry: %v\n", conn.ConnectionState().PeerCertificates[0].Issuer, expiry.Format(time.RFC850))
-
 	} else{
 		fmt.Println("Usage: check_ssl domain.tld")
 	 }
