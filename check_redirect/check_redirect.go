7f357c79722fb990ce4b587237f2cfbee54cd6b0
Hristo <jeleff@gmail.com>
2023-05-25 23:44:19 +0300

    follow redirects

--- check_redirect/check_redirect.go
+++ check_redirect/check_redirect.go
@@ -8,37 +8,49 @@ import (
 	"strings"
 )
 
+func check_redirect(url string)(string, int){
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+	client := new(http.Client)
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return errors.New("Redirect")
+	}
+
+	response, err := client.Do(req)
+	if err == nil {
+		fmt.Println(response.StatusCode, url)
+
+	} else {
+		
+		if strings.Contains(err.Error(), "Redirect") {
+			fmt.Println(response.StatusCode, response.Header.Get("Location"))
+		} else { 
+			fmt.Println(err) 
+		}
+	}
+	return response.Header.Get("Location"), response.StatusCode
+}
+
 func main() {
 	if len(os.Args) > 1 {
 		var domain string
-		if strings.HasPrefix(os.Args[1], "https://") {
+		if strings.HasPrefix(os.Args[1], "https://") || strings.HasPrefix(os.Args[1], "http://") {
 			domain = os.Args[1]
 		} else {
 			domain = "https://" + os.Args[1]
-		}
-		req, err := http.NewRequest("GET", domain, nil)
-		if err != nil {
-			panic(err)
-		}
-		client := new(http.Client)
-		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-			return errors.New("Redirect")
-		}
-
-		response, err := client.Do(req)
-		if err == nil {
-			fmt.Println(response.StatusCode, domain)
-
-		} else {
-			
-			if strings.Contains(err.Error(), "Redirect") {
-				fmt.Println(response.StatusCode, response.Header.Get("Location"))
-			} else { 
-				fmt.Println(err) 
+		}		
+		for {
+			resp_url, resp_code := check_redirect(domain)
+			domain = resp_url
+	
+			if resp_code < 300 || resp_code > 400 {
+				break
 			}
 		}
-
 	} else {
-		fmt.Printf("Usage: check_redirect domain.tld")
+		fmt.Printf("Usage: check_redirect domain.tld \n")
 	}
 }
